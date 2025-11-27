2ae9509f15bf33c56dc7d49a45d35f000c91c470
kartikbhadane15 <kartikbhadane004@gmail.com>
2025-11-27 23:44:07 +0530

    restructure reviews roter

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -37,49 +34,8 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-
-
-const valideteReview = (req, re, next) => {
-  let {error} = reviewSchema.validate(req.body);
-  // console.log(result);
-  if(error) {
-    let errMsg = error.details.map((el) => el.message).join(",")
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-}
-
 app.use("/listings", listings);
-
-
-//Reviews
-//Post Route
-app.post("/listings/:id/reviews", valideteReview, wrapAsync(async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
- listing.reviews.push(newReview);
-
- await newReview.save();
- await listing.save();
-
- res.redirect(`/listings/${listing._id}`);
-}));
-
-//Delete Route
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-  })
-);
-
+app.use("/listings/:id/reviews", reviews);
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -98,7 +54,6 @@ app.delete(
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
 });
-
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
